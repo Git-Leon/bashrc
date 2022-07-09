eebdc0f9ffa5f3e8580faf05714f75c53dbf050b
xleonhunter <xleonhunter@gmail.com>
2022-07-09 15:50:31 -0400

    updated gitignore

--- register-aliases.sh
+++ register-aliases.sh
@@ -5,4 +5,5 @@ alias cd='. ~/bashrc/aliases/change-directory.sh $1';
 alias git-acp=~/bashrc/aliases/git-acp.sh $*
 alias git-fall=~/bashrc/aliases/git-fetch-all.sh
 alias git-ignore=~/bashrc/aliases/git-ignore.sh
+alias git-reset=~/bashrc/aliases/git-reset.sh
 alias gitbash='C:/Program\ Files/Git/git-bash.exe'

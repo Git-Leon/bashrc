53753742900105d84f167a8fe8ef3dc603d2db88
xleonhunter <xleonhunter@gmail.com>
2022-07-10 09:32:23 -0400

    updated restart-process

--- aliases/find-replace.sh
+++ aliases/find-replace.sh
@@ -1,12 +1,12 @@
 findValue=$1
 replaceValue=$2
 inputValue=$3
-
 tmpFile=.tmp.txt
+
 echo $inputValue > $tmpFile
-sed -i -e "s+$findValue+$replaceValue+g" $tmpFile
+
 ESCAPED_REPLACE=$(printf '%s\n' "$findValue" | sed -e 's/[\/&]/\\&/g')
 ESCAPED_KEYWORD=$(printf '%s\n' "$findValue" | sed -e 's/[]\/$*.^[]/\\&/g');
 
-# Now you can use it inside the original sed statement to replace text
+sed -i -e "s+$findValue+$replaceValue+g" $tmpFile
 sed "s/$ESCAPED_KEYWORD/$ESCAPED_REPLACE/g" $tmpFile
--- aliases/restart-process.sh
+++ aliases/restart-process.sh
@@ -5,7 +5,9 @@ if [ "$1" ]
     script=$currentDirectory/batch/restart-process.bat    
     executablePathUnix="$($currentDirectory/batch/get-process-executable-path.bat $1)"
     executablePathDos=$(echo "$executablePathUnix" | tr -d '"')
-    executablePathDosEscaped=$(printf "%q" $executablePathDos)
+    executablePathDos="$($currentDirectory/find-replace.sh \\ \\\\ $executablePathDos)"
+    executablePathDos="$($currentDirectory/find-replace.sh \\\& \\\\\& $executablePathDos)"
+    executablePathDosEscaped="$($currentDirectory/find-replace.sh \\/ \\\\/ $executablePathDos)"
     ECHO $executablePathUnix
     ECHO $executablePathDos
     ECHO $executablePathDosEscaped

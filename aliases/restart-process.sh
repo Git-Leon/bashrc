1dc5a555c5b4843d0002e2c1e40d0660fe3e42ed
xleonhunter <xleonhunter@gmail.com>
2022-07-10 01:38:05 -0400

    updated restart-process.sh

--- aliases/restart-process.sh
+++ aliases/restart-process.sh
@@ -2,9 +2,13 @@
 if [ "$1" ]
   then
     currentDirectory=$HOME/bashrc/aliases
-    script=$currentDirectory/batch/restart-process.bat
-    executablePath="$($currentDirectory/batch/get-process-executable-path.bat $1)"
-    executablePath=$(echo "$executablePath" | tr -d '"')
+    script=$currentDirectory/batch/restart-process.bat    
+    executablePathUnix="$($currentDirectory/batch/get-process-executable-path.bat $1)"
+    executablePathDos=$(echo "$executablePathUnix" | tr -d '"')
+    executablePathDosEscaped=$(printf "%q" $executablePathDos)
+    ECHO $executablePathUnix
+    ECHO $executablePathDos
+    ECHO $executablePathDosEscaped
     cmd "/C $($currentDirectory/convert-path.sh $script) $1"
-    $executablePath &
+    $executablePathDosEscaped &
   fi

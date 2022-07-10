1c93d335836586a045785e8e776476eba49edaf5
xleonhunter <xleonhunter@gmail.com>
2022-07-10 01:20:50 -0400

    updated restart-process

--- aliases/batch/get-process-executable-path.bat
+++ aliases/batch/get-process-executable-path.bat
@@ -1,2 +1,2 @@
 @ECHO OFF                                                                              
-Wmic process where (Name like 'notepad.exe') get commandline | sed -n 2p
+Wmic process where (Name like '%~1') get commandline | sed -n 2p
--- aliases/restart-process.sh
+++ aliases/restart-process.sh
@@ -3,5 +3,8 @@ if [ "$1" ]
   then
     currentDirectory=$HOME/bashrc/aliases
     script=$currentDirectory/batch/restart-process.bat
+    executablePath="$($currentDirectory/batch/get-process-executable-path.bat $1)"
+    executablePath=$(echo "$executablePath" | tr -d '"')
     cmd "/C $($currentDirectory/convert-path.sh $script) $1"
+    $executablePath &
   fi

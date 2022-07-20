3293766e5b49b3ba09d1afddb729aa9ab85058ce
xleonhunter <xleonhunter@gmail.com>
2022-07-20 10:16:07 -0400

    created debloat script

---
+++ aliases/kill-bloat.sh
@@ -0,0 +1,19 @@
+killScript=$HOME/bashrc/aliases/kill-process.sh  
+
+$killScript code.exe
+$killScript notepad++.exe
+$killScript discord.exe
+$killScript mspaint.exe
+
+$killScript "LITSSvc.exe"
+$killScript "ibmpmsvc.exe"
+$killScript "PowerMgr.exe"
+$killScript "ApsInsSvc.exe"
+$killScript "EasyResume.exe"
+$killScript "Lenovo.Modern.ImController.exe"
+$killScript "Lenovo.Modern.ImController.PluginHost.exe"
+$killScript "Lenovo.Modern.ImController.PluginHost.Device.exe"
+$killScript "Lenovo.Modern.ImController.PluginHost.SettingsApp.exe"
+
+# start "gitbash" "C:/Program Files/Git/git-bash.exe"
+$killScript bash.exe

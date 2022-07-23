0492eed1b58df6f2d97478d70a92ee31a16aa99a
xleonhunter <xleonhunter@gmail.com>
2022-07-23 07:19:09 -0400

    updated git-reset

--- aliases/git-reset.sh
+++ aliases/git-reset.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-git stash
-git reset --hard
-git fetch --all
-git pull --all
-git branch
-
 if [ "$1" ]
   then
     git reset --hard HEAD~$1
-  fi
+  else  
+    git stash
+    git reset --hard
+    git fetch --all
+    git pull --all
+    git branch
+fi

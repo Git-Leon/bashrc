04b5dba009da4a86af67065f917e0782e3c3a583
xleonhunter <xleonhunter@gmail.com>
2022-07-24 08:40:47 -0400

    updated unjar-to-java

--- aliases/unjar-to-java.sh
+++ aliases/unjar-to-java.sh
@@ -7,5 +7,9 @@ if [ "$1" ]
         cp $1 unjarred/$directoryName/$1
         cd unjarred/$directoryName
         jar xf $1
-        javap -classpath $1 -c $(jar -tf $1 | grep class | sed 's/.class//g')
+        classes=$(jar -tf $1 | grep class | sed 's/.class//g')
+        echo $classes > .tmp.txt
+        for class in $(cat .tmp.txt); do javap -classpath $1 -c $class; done
+        cat .tmp.txt
+        rm .tmp.txt
     fi

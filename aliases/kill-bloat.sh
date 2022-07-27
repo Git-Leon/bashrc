b0efaf1eb32f4432d323494a39c6f8d2168584a7
leon <xleonhunter@gmail.com>
2022-07-26 20:46:15 -0400

    updated kill-bloat script

--- aliases/kill-bloat.sh
+++ aliases/kill-bloat.sh
@@ -1,22 +1,22 @@
 killScript=$HOME/bashrc/aliases/kill-process.sh  
 
 
-$killScript code.exe
-$killScript notepad++.exe
-$killScript discord.exe
-$killScript mspaint.exe
-$killScript idea64.exe
-$killScript java.exe
+$killScript code
+$killScript notepad++
+$killScript discord
+$killScript mspaint
+$killScript idea64
+$killScript java
 
-$killScript "LITSSvc.exe"
-$killScript "ibmpmsvc.exe"
-$killScript "PowerMgr.exe"
-$killScript "ApsInsSvc.exe"
-$killScript "EasyResume.exe"
-$killScript "Lenovo.Modern.ImController.exe"
-$killScript "Lenovo.Modern.ImController.PluginHost.exe"
-$killScript "Lenovo.Modern.ImController.PluginHost.Device.exe"
-$killScript "Lenovo.Modern.ImController.PluginHost.SettingsApp.exe"
+$killScript "LITSSvc"
+$killScript "ibmpmsvc"
+$killScript "PowerMgr"
+$killScript "ApsInsSvc"
+$killScript "EasyResume"
+$killScript "Lenovo.Modern.ImController"
+$killScript "Lenovo.Modern.ImController.PluginHost"
+$killScript "Lenovo.Modern.ImController.PluginHost.Device"
+$killScript "Lenovo.Modern.ImController.PluginHost.SettingsApp"
 
-# start "gitbash" "C:/Program Files/Git/git-bash.exe"
-$killScript bash.exe
+# start "gitbash" "C:/Program Files/Git/git-bash"
+$killScript bash
--- aliases/kill-process.sh
+++ aliases/kill-process.sh
@@ -4,7 +4,7 @@ if [ "$1" ]
     { #try
       currentDirectory=$HOME/bashrc/aliases
       script=$currentDirectory/batch/kill-process.bat
-      cmd "/C $($currentDirectory/convert-path.sh $script) $1" 2>null
+      cmd "/C $($currentDirectory/convert-path.sh $script) $1.exe" 2>null
     } || { #catch      
       ps -ef | grep $1 | grep -v grep | awk '{print $2}' | xargs -r kill -9
     }

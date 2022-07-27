8f4d7d77fd9b61ee8aede446c55b768c914a47d7
leon <xleonhunter@gmail.com>
2022-07-26 20:34:58 -0400

    updated kill-process script

--- aliases/kill-process.sh
+++ aliases/kill-process.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 if [ "$1" ]
   then
-    currentDirectory=$HOME/bashrc/aliases
-    script=$currentDirectory/batch/kill-process.bat
-    cmd "/C $($currentDirectory/convert-path.sh $script) $1"
+    { #try
+      currentDirectory=$HOME/bashrc/aliases
+      script=$currentDirectory/batch/kill-process.bat
+      cmd "/C $($currentDirectory/convert-path.sh $script) $1" 2>null
+    } || { #catch      
+      ps -ef | grep $1 | grep -v grep | awk '{print $2}' | xargs -r kill -9
+    }
   fi

78d33f571e0763816f0fa58b23a5eb498c9d068b
xleonhunter <xleonhunter@gmail.com>
2022-07-27 17:18:24 -0400

    updated kill-port script

--- aliases/kill-port.sh
+++ aliases/kill-port.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-if [ "$1" ]
-  then
-    portNumbers=$(netstat -a -n -b -o | grep $1 | sed -e "s/[[:space:]]\+/ /g" | cut -d ' ' -f6)        
-    { #try 
-        for portNumber in $portNumbers; do
-          taskkill /PID /F "$portNumber" 2>/dev/null
-        done 
-    } || { # catch
-        for portNumber in $portNumbers; do
-          taskkill //F //PID "$portNumber"
-        done 
-    }
-fi
+killPort() {
+  if [ "$1" ]
+    then
+    echo "Attempting to kill process on $1"
+      processIds=$(netstat -a -n -b -o | grep $1 | sed -e "s/[[:space:]]\+/ /g" | cut -d ' ' -f6)
+      for processId in $processIds; do
+        { #try 
+          echo "Attempting to kill process on $1, using BASH syntax"
+          taskkill /PID /F "$processId" 2>null
+          echo "Process on $1 killed."
+          return
+        } || { # catch
+          echo "Attempting to kill process on $1, using cygwin syntax"
+          taskkill //F //PID "$processId"
+          echo "Process on $1 killed."
+          return
+        }
+      done
+      echo "Process on $1 NOT killed."
+  fi
+}
+
+killPort $1

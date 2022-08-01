dcf779d26a5da1117749ac68967469ec313e74ed
xleonhunter <xleonhunter@gmail.com>
2022-08-01 11:09:48 -0400

    updated generate-gitignore and kill-port.sh

--- aliases/generate-gitignore.sh
+++ aliases/generate-gitignore.sh
@@ -1,1 +1,1 @@
-curl https://www.toptal.com/developers/gitignore/api/visualstudio,visualstudiocode,intellij,rider,notepadpp,sublimetext,windows,macos,linux,pycharm,jetbrains+all,netbeans,eclipse,unity,java,maven,gradle,node > .gitignore
+curl https://www.toptal.com/developers/gitignore/api/visualstudio,visualstudiocode,intellij,rider,notepadpp,sublimetext,windows,macos,linux,pycharm,jetbrains+all,netbeans,eclipse,unity,java,maven,gradle,node,angular,flask,reactnative,ionic3 > .gitignore
--- aliases/kill-port.sh
+++ aliases/kill-port.sh
@@ -2,14 +2,14 @@
 if [ "$1" ]
   then
     processIds=$(netstat -a -n -b -o | grep $1 | sed -e "s/[[:space:]]\+/ /g" | cut -d ' ' -f6)        
-    echo "Attempting to kill process running port number $1"
+    echo "Attempting to kill process running on port number $1"
     for processId in $processIds; do
       { #try 
-          echo "Attempting to kill process running port number $1 using CYGWIN syntax"
+          echo "Attempting to kill process running on port number $1 using CYGWIN syntax"
             taskkill //F //PID "$processId"
           echo "Process on $1 killed."
       } || { # catch
-          echo "Attempting to kill process running port number $1 using BASH syntax"        
+          echo "Attempting to kill process running on port number $1 using BASH syntax"
             taskkill /F /PID "$processId" 
           echo "Process on $1 killed."
       } || {

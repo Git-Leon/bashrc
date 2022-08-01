a9e6688992e86ee56b7bc28f3415cb09285f4ba1
xleonhunter <xleonhunter@gmail.com>
2022-08-01 10:10:04 -0400

    updated variable named in kill-port

--- aliases/kill-port.sh
+++ aliases/kill-port.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$1" ]
   then
-    portNumbers=$(netstat -a -n -b -o | grep $1 | sed -e "s/[[:space:]]\+/ /g" | cut -d ' ' -f6)        
+    processIds=$(netstat -a -n -b -o | grep $1 | sed -e "s/[[:space:]]\+/ /g" | cut -d ' ' -f6)        
     echo "Attempting to kill process running port number $1"
-    for portNumber in $portNumbers; do
+    for processId in $processIds; do
       { #try 
           echo "Attempting to kill process running port number $1 using CYGWIN syntax"
-            taskkill //F //PID "$portNumber"
+            taskkill //F //PID "$processId"
           echo "Process on $1 killed."
       } || { # catch
           echo "Attempting to kill process running port number $1 using BASH syntax"        
-            taskkill /F /PID "$portNumber" 
+            taskkill /F /PID "$processId" 
           echo "Process on $1 killed."
       } || {
           echo "Process on $1 NOT killed."

9cc3158f53910ada9c8f43cf2af0249f412e0bf7
xleonhunter <xleonhunter@gmail.com>
2022-08-15 17:37:21 -0400

    updated git-mgirate

--- aliases/git-migrate.sh
+++ aliases/git-migrate.sh
@@ -1,15 +1,22 @@
 sourceRepo=$1
 targetRepo=$2
-destDirectory="$(date | awk -F':' '{print $1}')"
+destDirectory=$(date +%s)
 
 git clone $sourceRepo $destDirectory
 cd $destDirectory
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+git branch -r | grep -v '\->' |\
+while read remote; do
+    git branch --track "${remote#origin/}" "$remote"; done
 git fetch --all
 git pull --all
 git branch
 git remote add new-origin $targetRepo
-git remote --all new-origin
 git push --all new-origin -f
 git remote rm origin
 git remote rename new-origin origin
+cd ..
+rm -rf $destDirectory
+
+echo "Successfully migrated!"
+echo "source: [ $sourceRepo ]"
+echo "source: [ $targetRepo ]"

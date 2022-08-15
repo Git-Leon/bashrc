ae4b95d2465f628a606fc25bbfa7ffadf3e84d04
xleonhunter <xleonhunter@gmail.com>
2022-08-14 20:38:59 -0400

    updated git-fork-sync

--- aliases/git-fork-sync.sh
+++ aliases/git-fork-sync.sh
@@ -7,7 +7,11 @@ git pull $url
 git remote add t$hostName git@github.com:$hostName/$repoName.git
 git fetch $hostName
 
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+git branch -r | grep -v '\->' |\
+while read remote; do
+    git branch --track "${remote#origin/}" "$remote";
+    git pull
+done
 git fetch --all
 git pull --all
 git branch

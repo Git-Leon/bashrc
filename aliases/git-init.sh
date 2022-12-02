fb73e04f317de3e02f275fb73478e01e670e510d
git-leon <xleonhunter@gmail.com>
2022-12-02 16:42:18 -0500

    updated git-init script to add origin

--- aliases/git-init.sh
+++ aliases/git-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 $SCRIPTPATH/get-gitignore.sh
 
@@ -9,3 +9,9 @@ git init
 git add .
 git commit -m "$commitMessage"
 gh repo create $repoName --public --source=. --remote=upstream --push
+origin=$(git remote -v | grep fetch)
+origin=$(echo ${origin/"upstream        "/""})
+origin=$(echo ${origin/"upstream"/""})
+origin=$(echo ${origin// (fetch)/})
+
+git remote add origin $origin

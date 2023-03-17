93517e1ee65068fce0794f4bd6295d72069a23b1
git-leon <xleonhunter@gmail.com>
2023-03-17 09:59:14 -0400

    updated kill-bloat script

--- aliases/kill-bloat.sh
+++ aliases/kill-bloat.sh
@@ -17,6 +17,22 @@ $killScript "Lenovo.Modern.ImController"
 $killScript "Lenovo.Modern.ImController.PluginHost"
 $killScript "Lenovo.Modern.ImController.PluginHost.Device"
 $killScript "Lenovo.Modern.ImController.PluginHost.SettingsApp"
+$killScript "Zoom"
+$killScript "Descript"
+$killScript "Recut"
+
+# Webex
+$killScript "CiscoCollabHost"
+$killScript "WebexHost"
+$killScript "wmlhost"
+
+# Citrix
+$killScript "AuthManSvr"
+$killScript "concentr"
+$killScript "wfcrun32"
+$killScript "SelfServicePlugin"
+$killScript "Receiver"
+$killScript "CDViewer"
 
 # start "gitbash" "C:/Program Files/Git/git-bash"
 $killScript bash

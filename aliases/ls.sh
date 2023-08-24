f1550c2226133d268338a635698bc62d8c66024c
git-leon <xleonhunter@gmail.com>
2023-08-24 12:15:57 -0400

    updated ls.sh

--- aliases/ls.sh
+++ aliases/ls.sh
@@ -1,5 +1,5 @@
 { # linux and cygwin environment
-    ls -F --color=auto --show-control-chars 2>/dev/null
+    ls -F --color=auto --show-control-chars 2>/dev/null $*
 } || { # mac and unix environment
-    ls -F -G 2>/dev/null
+    ls -F -G 2>/dev/null $*
 }

6ba344c013f77fe3c2232b80e884763476e1d9e4
git-leon <xleonhunter@gmail.com>
2023-09-20 16:06:35 -0400

    updated color-echo.sh

--- aliases/color-echo.sh
+++ aliases/color-echo.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-declare -A colors
+declare -A colorMap
 
-colors['off']='\033[0m'
-colors['black']='\033[30m'
-colors['red']='\033[31m'
-colors['green']='\033[32m'
-colors['yellow']='\033[33m'
-colors['blue']='\033[34m'
-colors['purple']='\033[35m'
-colors['cyan']='\033[36m'
-colors['white']='\033[37m'
+colorMap['off']='\033[0m'
+colorMap['black']='\033[30m'
+colorMap['red']='\033[31m'
+colorMap['green']='\033[32m'
+colorMap['yellow']='\033[33m'
+colorMap['blue']='\033[34m'
+colorMap['purple']='\033[35m'
+colorMap['cyan']='\033[36m'
+colorMap['white']='\033[37m'
 
-colors['light-black']='\033[1;30m'
-colors['light-red']='\033[1;31m'
-colors['light-green']='\033[1;32m'
-colors['light-yellow']='\033[1;33m'
-colors['light-blue']='\033[1;34m'
-colors['light-purple']='\033[1;35m'
-colors['light-cyan']='\033[1;36m'
-colors['light-white']='\033[1;37m'
+colorMap['light-black']='\033[1;30m'
+colorMap['light-red']='\033[1;31m'
+colorMap['light-green']='\033[1;32m'
+colorMap['light-yellow']='\033[1;33m'
+colorMap['light-blue']='\033[1;34m'
+colorMap['light-purple']='\033[1;35m'
+colorMap['light-cyan']='\033[1;36m'
+colorMap['light-white']='\033[1;37m'
 
-defaultColor=${colors['off']}
-color=${colors[$1{1,,}]}
+defaultColor=${colorMap['off']}
+selectedColor=${colorMap[$1{1,,}]}
 contentToEcho=("${@:2}")
-echo -e "$color$contentToEcho$defaultColor"
+echo -e "$selectedColor$contentToEcho$defaultColor"

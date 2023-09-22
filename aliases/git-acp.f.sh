c802528ed01aae1ac3d0bc7ee7a4180e521859d9
git-leon <xleonhunter@gmail.com>
2023-09-22 14:52:44 -0400

    updated git-acp.f

--- aliases/git-acp.f.sh
+++ aliases/git-acp.f.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo git config --global --add --bool push.autoSetupRemote true
 git config --global --add --bool push.autoSetupRemote true
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
@@ -7,4 +8,5 @@ $SCRIPT_DIR/git-ac.sh $*
 echo git push -f
 git push -f
 
+echo git config --global --add --bool push.autoSetupRemote false
 git config --global --add --bool push.autoSetupRemote false

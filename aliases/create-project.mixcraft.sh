915078a6b217f125b021256f75e2a3b856b14b53
git-leon <xleonhunter@gmail.com>
2023-09-23 12:01:14 -0400

    updated create-project.mixcraft.sh

--- aliases/create-project.mixcraft.sh
+++ aliases/create-project.mixcraft.sh
@@ -3,5 +3,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 commitMessage="${@:2}"
 if [ "$1" ] && [ "$commitMessage" ]
     then
+        git clone https://github.com/cymatic-productions/mixcraft.projecttemplate $1
+        cd $1
+        rm -rf .git
         $SCRIPT_DIR/git-init.sh cymatic-productions/$1 $commitMessage
 fi
